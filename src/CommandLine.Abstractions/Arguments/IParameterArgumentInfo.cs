f0d87bc6aed8d14d4228021f38e5e14204171a49
Nightowl <nightowl286@protonmail.com>
2025-07-13 05:45:14 +0100

    + Add initial argument abstractions.

---
+++ src/CommandLine.Abstractions/Arguments/IParameterArgumentInfo.cs
@@ -0,0 +1,20 @@
+namespace OwlDomain.CommandLine.Arguments;
+
+/// <summary>
+/// 	Represents information about an argument that is linked to a parameter.
+/// </summary>
+public interface IParameterArgumentInfo : IArgumentInfo
+{
+	#region Properties
+	Type IArgumentInfo.ValueType => Parameter.ParameterType;
+
+	/// <summary>The parameter that represents the argument.</summary>
+	ParameterInfo Parameter { get; }
+	#endregion
+}
+
+/// <summary>
+/// 	Represents information about an argument that is linked to a parameter.
+/// </summary>
+/// <typeparam name="T">The type of the argument's value.</typeparam>
+public interface IParameterArgumentInfo<out T> : IParameterArgumentInfo, IArgumentInfo<T> { }
