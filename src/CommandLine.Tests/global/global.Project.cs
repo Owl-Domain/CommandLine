6f90185c74a3ace74f3e521d701f10f75a78272e
Nightowl <nightowl286@protonmail.com>
2025-08-02 23:52:27 +0100

    Add empty unit test project.

---
+++ src/CommandLine.Tests/global/global.Project.cs
@@ -0,0 +1,13 @@
+global using OwlDomain.CommandLine.Arguments;
+global using OwlDomain.CommandLine.Flags;
+global using OwlDomain.CommandLine.Commands;
+global using OwlDomain.CommandLine.Groups;
+global using OwlDomain.CommandLine.Parsing;
+global using OwlDomain.CommandLine.Diagnostics;
+global using OwlDomain.CommandLine.Parsing.Tree;
+global using OwlDomain.CommandLine.Parsing.Values;
+global using OwlDomain.CommandLine.Validation;
+global using OwlDomain.CommandLine.Execution;
+
+
+global using DiagnosticSource = OwlDomain.CommandLine.Diagnostics.DiagnosticSource;
