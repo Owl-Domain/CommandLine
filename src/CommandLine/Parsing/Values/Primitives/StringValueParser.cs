87b0b65ba60634814bd86f94617ac6d08c249b88
Nightowl <nightowl286@protonmail.com>
2025-08-07 04:35:39 +0100

    Add single quoted string value parsing.

--- src/CommandLine/Parsing/Values/Primitives/StringValueParser.cs
+++ src/CommandLine/Parsing/Values/Primitives/StringValueParser.cs
@@ -18,55 +18,101 @@ public sealed class StringValueParser : BaseValueParser<string>
 		}
 
 		if (parser.Match('"'))
-		{
-			StringBuilder builder = new();
+			return TryParseDoubleQuote(parser, out error);
 
-			bool closed = false;
-			while (parser.Current is not '\0')
-			{
-				if (parser.Match('"'))
-				{
-					closed = true;
-					break;
-				}
-
-				if (parser.Match('\\', '"')) builder.Append('"');
-				else if (parser.Match('\\', '\\')) builder.Append('\\');
-				else if (parser.Match('\\', 'a')) builder.Append('\a');
-				else if (parser.Match('\\', 'b')) builder.Append('\b');
-				else if (parser.Match('\\', 't')) builder.Append('\t');
-				else if (parser.Match('\\', 'n')) builder.Append('\n');
-				else if (parser.Match('\\', 'f')) builder.Append('\f');
-				else if (parser.Match('\\', 'e')) builder.Append('\e');
-				else if (parser.Match('\\'))
-				{
-					if (parser.Current is not '\0')
-					{
-						builder.Append(parser.Current);
-						parser.Advance();
-					}
-
-					continue;
-				}
-
-				builder.Append(parser.Current);
-				parser.Advance();
-			}
-
-			if (closed is false)
-			{
-				error = "Quoted string was unclosed.";
-				return default;
-			}
-
-			error = default;
-			return builder.ToString();
-		}
+		if (parser.Match('\''))
+			return TryParseSingleQuote(parser, out error);
 
 		string value = parser.AdvanceUntilBreak();
 
 		error = value.Length is 0 ? string.Empty : null;
 		return value;
+	}
+	#endregion
+
+	#region Helpers
+	private static string? TryParseDoubleQuote(ITextParser parser, out string? error)
+	{
+		StringBuilder builder = new();
+
+		bool closed = false;
+		while (parser.Current is not '\0')
+		{
+			if (parser.Match('"'))
+			{
+				closed = true;
+				break;
+			}
+
+			if (Escape(parser, builder))
+				continue;
+
+			builder.Append(parser.Current);
+			parser.Advance();
+		}
+
+		if (closed is false)
+		{
+			error = "Quoted string was unclosed.";
+			return default;
+		}
+
+		error = default;
+		return builder.ToString();
+	}
+	private static string? TryParseSingleQuote(ITextParser parser, out string? error)
+	{
+		StringBuilder builder = new();
+
+		bool closed = false;
+		while (parser.Current is not '\0')
+		{
+			if (parser.Match('\''))
+			{
+				closed = true;
+				break;
+			}
+
+			if (Escape(parser, builder))
+				continue;
+
+			builder.Append(parser.Current);
+			parser.Advance();
+		}
+
+		if (closed is false)
+		{
+			error = "Quoted string was unclosed.";
+			return default;
+		}
+
+		error = default;
+		return builder.ToString();
+	}
+	private static bool Escape(ITextParser parser, StringBuilder builder)
+	{
+		if (parser.Current is not '\\')
+			return false;
+
+		if (parser.Match('\\', '\\')) builder.Append('\\');
+		else if (parser.Match('\\', '"')) builder.Append('"');
+		else if (parser.Match('\\', '\'')) builder.Append('\'');
+		else if (parser.Match('\\', 'a')) builder.Append('\a');
+		else if (parser.Match('\\', 'b')) builder.Append('\b');
+		else if (parser.Match('\\', 't')) builder.Append('\t');
+		else if (parser.Match('\\', 'n')) builder.Append('\n');
+		else if (parser.Match('\\', 'f')) builder.Append('\f');
+		else if (parser.Match('\\', 'e')) builder.Append('\e');
+		else if (parser.Match('\\'))
+		{
+			if (parser.Current is not '\0')
+			{
+				builder.Append(parser.Current);
+				parser.Advance();
+			}
+		}
+
+		return true;
 	}
 	#endregion
 }
