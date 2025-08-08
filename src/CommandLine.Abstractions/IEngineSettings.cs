26f878f3d8de8f804c4c879e45450f3215f76576
Nightowl <nightowl286@protonmail.com>
2025-08-08 06:49:01 +0100

    Add the intial settings (no effect yet).

--- src/CommandLine.Abstractions/IEngineSettings.cs
+++ src/CommandLine.Abstractions/IEngineSettings.cs
@@ -6,5 +6,50 @@ namespace OwlDomain.CommandLine;
 public interface IEngineSettings
 {
 	#region Properties
+	/// <summary>Whether flag shadowing is allowed.</summary>
+	/// <remarks>
+	/// 	This indicates whether the flags from outer command groups can be shadows
+	/// 	by different flags from inner command groups when they have the same name.
+	/// </remarks>
+	bool AllowFlagShadowing { get; }
+
+	/// <summary>The prefix to use for detecting long name flags.</summary>
+	/// <remarks>
+	/// 	If this is the same as <see cref="ShortFlagPrefix"/> then
+	/// 	<see cref="MergeLongAndShortFlags"/> must be set to <see langword="true"/>.
+	/// </remarks>
+	string LongFlagPrefix { get; }
+
+	/// <summary>The prefix to use for detecting short name flags.</summary>
+	/// <remarks>
+	/// 	If this is the same as <see cref="LongFlagPrefix"/> then
+	/// 	<see cref="MergeLongAndShortFlags"/> must be set to <see langword="true"/>.
+	/// </remarks>
+	string ShortFlagPrefix { get; }
+
+	/// <summary>Whether long and short flags should have the same prefix.</summary>
+	bool MergeLongAndShortFlags { get; }
+
+	/// <summary>Whether a virtual help flag should be included in every command.</summary>
+	/// <remarks>
+	/// 	If this is set to <see langword="true"/> then either <see cref="LongHelpFlagName"/>
+	/// 	or <see cref="ShortHelpFlagName"/> (or both), must have a value.
+	/// </remarks>
+	bool IncludeHelpFlag { get; }
+
+	/// <summary>The long name for the virtual help flag.</summary>
+	/// <remarks><see cref="IncludeHelpFlag"/> must be set to <see langword="true"/> for this setting to matter.</remarks>
+	string? LongHelpFlagName { get; }
+
+	/// <summary>The short name for the virtual help flag.</summary>
+	/// <remarks><see cref="IncludeHelpFlag"/> must be set to <see langword="true"/> for this setting to matter.</remarks>
+	char? ShortHelpFlagName { get; }
+
+	/// <summary>Whether the help command should be included in every command group.</summary>
+	bool IncludeHelpCommand { get; }
+
+	/// <summary>The name of the virtual help command.</summary>
+	/// <remarks><see cref="IncludeHelpCommand"/> must be set to <see langword="true"/> for this setting to matter.</remarks>
+	string HelpCommandName { get; }
 	#endregion
 }
--- src/CommandLine/EngineSettings.cs
+++ src/CommandLine/EngineSettings.cs
@@ -5,5 +5,32 @@ namespace OwlDomain.CommandLine;
 /// </summary>
 public sealed class EngineSettings : IEngineSettings
 {
+	#region Properties
+	/// <inheritdoc/>
+	public required bool AllowFlagShadowing { get; init; }
 
+	/// <inheritdoc/>
+	public required string LongFlagPrefix { get; init; }
+
+	/// <inheritdoc/>
+	public required string ShortFlagPrefix { get; init; }
+
+	/// <inheritdoc/>
+	public required bool MergeLongAndShortFlags { get; init; }
+
+	/// <inheritdoc/>
+	public required bool IncludeHelpFlag { get; init; }
+
+	/// <inheritdoc/>
+	public required string? LongHelpFlagName { get; init; }
+
+	/// <inheritdoc/>
+	public required char? ShortHelpFlagName { get; init; }
+
+	/// <inheritdoc/>
+	public required bool IncludeHelpCommand { get; init; }
+
+	/// <inheritdoc/>
+	public required string HelpCommandName { get; init; }
+	#endregion
 }
