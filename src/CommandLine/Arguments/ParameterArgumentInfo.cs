c2ea6eb125cc539c974f604418be76afe93fe438
Nightowl <nightowl286@protonmail.com>
2025-08-10 05:50:56 +0100

    Add the BaseArgumentInfo class.

--- src/CommandLine/Arguments/ParameterArgumentInfo.cs
+++ src/CommandLine/Arguments/ParameterArgumentInfo.cs
@@ -4,70 +4,28 @@ namespace OwlDomain.CommandLine.Arguments;
 /// 	Represents information about an argument that is linked to a parameter.
 /// </summary>
 /// <typeparam name="T">The type of the argument's value.</typeparam>
-[DebuggerDisplay($"{{{nameof(DebuggerDisplay)}(), nq}}")]
-public sealed class ParameterArgumentInfo<T> : IParameterArgumentInfo<T>
+/// <param name="parameter">The parameter that represents the argument.</param>
+/// <param name="name">The name of the argument.</param>
+/// <param name="position">The position of the argument.</param>
+/// <param name="isRequired">Whether the argument has to be set when executing the command.</param>
+/// <param name="defaultValue">The default value for the argument.</param>
+/// <param name="parser">The value parser selected for the argument.</param>
+/// <param name="documentation">The documentation for the argument.</param>
+/// <param name="defaultValueLabel">The label for the <paramref name="defaultValue"/>.</param>
+public sealed class ParameterArgumentInfo<T>(
+	ParameterInfo parameter,
+	string name,
+	int position,
+	bool isRequired,
+	T? defaultValue,
+	IValueParser<T> parser,
+	IDocumentationInfo? documentation,
+	string? defaultValueLabel)
+	: BaseArgumentInfo<T>(name, position, isRequired, defaultValue, parser, documentation, defaultValueLabel), IParameterArgumentInfo<T>
 {
 	#region Properties
 	/// <inheritdoc/>
-	public ParameterInfo Parameter { get; }
-
-	/// <inheritdoc/>
-	public string Name { get; }
-
-	/// <inheritdoc/>
-	public int Position { get; }
-
-	/// <inheritdoc/>
-	public bool IsRequired { get; }
-
-	/// <inheritdoc/>
-	public T? DefaultValue { get; }
-
-	/// <inheritdoc/>
-	public IValueParser<T> Parser { get; }
-
-	/// <inheritdoc/>
-	public IDocumentationInfo? Documentation { get; }
-
-	/// <inheritdoc/>
-	public string? DefaultValueLabel { get; }
-	#endregion
-
-	#region Constructors
-	/// <summary>Creates a new instance of the <see cref="ParameterArgumentInfo{T}"/>.</summary>
-	/// <param name="parameter">The parameter that represents the argument.</param>
-	/// <param name="name">The name of the argument.</param>
-	/// <param name="position">The position of the argument.</param>
-	/// <param name="isRequired">Whether the argument has to be set when executing the command.</param>
-	/// <param name="defaultValue">The default value for the argument.</param>
-	/// <param name="parser">The value parser selected for the argument.</param>
-	/// <param name="documentation">The documentation for the argument.</param>
-	/// <param name="defaultValueLabel">The label for the <paramref name="defaultValue"/>.</param>
-	public ParameterArgumentInfo(
-		ParameterInfo parameter,
-		string name,
-		int position,
-		bool isRequired,
-		T? defaultValue,
-		IValueParser<T> parser,
-		IDocumentationInfo? documentation,
-		string? defaultValueLabel)
-	{
-		name.ThrowIfEmptyOrWhitespace(nameof(name));
-		position.ThrowIfLessThan(0, nameof(position));
-
-		if (isRequired && defaultValue is not null)
-			Throw.New.ArgumentException(nameof(defaultValue), "Default values are not allowed for required arguments.");
-
-		Parameter = parameter;
-		Name = name;
-		Position = position;
-		IsRequired = isRequired;
-		DefaultValue = defaultValue;
-		Parser = parser;
-		Documentation = documentation;
-		DefaultValueLabel = defaultValueLabel ?? (isRequired ? null : defaultValue?.ToString());
-	}
+	public ParameterInfo Parameter { get; } = parameter;
 	#endregion
 
 	#region Methods
@@ -98,10 +56,5 @@ public sealed class ParameterArgumentInfo<T> : IParameterArgumentInfo<T>
 	{
 		return Parameter.TryGetCustomAttributes(out attributes);
 	}
-	#endregion
-
-	#region Helpers
-	[ExcludeFromCodeCoverage]
-	private string DebuggerDisplay() => $"Argument {{ Name = ({Name}), ValueType = ({typeof(T)}) }}";
 	#endregion
 }
